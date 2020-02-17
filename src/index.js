b978df52978ed5ed8edf9ce715d405a1935aae40
Joanna Chądzyńska <jmalek87@gmail.com>
2020-02-17 18:57:25 +0100

    blah

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./components/App";
+
 import * as serviceWorker from "./serviceWorker";
 
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<PersistGate persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>,
+	document.getElementById("root")
 );
 
 serviceWorker.unregister();
